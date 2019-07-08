2b80f688e7b38a16d74fa54a0133a37ab7f9150e
osidekyle <47363388+osidekyle@users.noreply.github.com>
2019-07-07 19:10:33 -0700

    Update elemental_quiz.py

--- elemental_quiz.py
+++ elemental_quiz.py
@@ -1,13 +1,9 @@
-#!/usr/bin/env python
-# coding: utf-8
 
-# In[40]:
 
 
 entered = []
 
 
-# In[41]:
 
 
 def answer_getter():
@@ -19,33 +15,26 @@ def answer_getter():
             entered.append(enter.lower())
 
 
-# In[43]:
 
 
 print("Enter the name of 5 of the first 20 elements on the periodic table of elements\n")
 answer_getter()
 
 
-# In[44]:
 
 
 get_ipython().system('curl https://raw.githubusercontent.com/MicrosoftLearning/intropython/master/elements1_20.txt -o elements1_20.txt')
 
 
-# In[45]:
 
 
 twenty_elements = open("elements1_20.txt","r")
 
 
-# In[46]:
 
 
 true_elems=[]
 one_elem = ''
-
-
-# In[47]:
 
 
 for item in range(20):
@@ -53,20 +42,17 @@ for item in range(20):
     true_elems.append(one_elem.strip().lower())
 
 
-# In[48]:
 
 
 print(true_elems)
 
 
-# In[49]:
 
 
 correcto = []
 incorrecto = []
 
 
-# In[50]:
 
 
 for words in entered:
@@ -76,14 +62,12 @@ for words in entered:
         incorrecto.append(words)
 
 
-# In[51]:
 
 
 
 percen = len(correcto)*20
 
 
-# In[52]:
 
 
 print("Quiz score:",percen,"% correct\n")
